2166d4e5a4bc42a27058413230f49ca12ec306c2
Ryan Alves <ra546038@gmail.com>
2023-03-31 16:50:10 -0300

    Initial commit

---
+++ src/br/senai/sp/jandira/Subtracao.java
@@ -0,0 +1,28 @@
+package br.senai.sp.jandira;
+
+import javax.swing.JOptionPane;
+
+public class Subtracao {
+
+	public static void main(String[] args) {
+
+		String strValor1, strValor2;
+		double valor1, valor2, soma;
+		
+		strValor1 = JOptionPane.showInputDialog("Digite o primeiro valor");
+		strValor2 = JOptionPane.showInputDialog("Digite o segundo valor");
+		
+		valor1 = Double.parseDouble(strValor1);
+		valor2 = Double.parseDouble(strValor2);
+		
+		if (valor1 <= valor2) {
+			soma = valor2 - valor1;
+		}else {
+			soma = valor1 - valor2;
+		}
+		
+		JOptionPane.showMessageDialog(null, "O resultado da subtração é: " + soma);
+	
+	}
+
+}
